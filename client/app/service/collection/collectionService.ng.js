8f2d81caabc9c0f716e0efd5dadc799f32037573
Alexis on Mac <entraide.community.developer@gmail.com>
2015-09-29 08:16:59 +0200

    subscription collection service

---
+++ client/app/service/collection/collectionService.ng.js
@@ -0,0 +1,12 @@
+angular.module("entraide").factory("CollectionService", function(){
+
+    var collectionService = {
+        subscriptions : [],
+
+        get : function(subscription){
+
+        }
+    };
+
+    return collectionService;
+});
