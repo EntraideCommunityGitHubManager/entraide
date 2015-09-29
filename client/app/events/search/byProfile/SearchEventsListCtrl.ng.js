00e070e5d5f8137c6370596932cd90816c4643c4
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-09-29 11:31:13 +0200

    Update SearchEventsListCtrl.ng.js

--- client/app/events/search/byProfile/SearchEventsListCtrl.ng.js
+++ client/app/events/search/byProfile/SearchEventsListCtrl.ng.js
@@ -1,13 +1,17 @@
-angular.module('entraide').controller('SearchEventsListCtrl', function ($scope, $meteor) {
+angular.module('entraide').controller('SearchEventsListCtrl', function ($scope, $meteor, CollectionService) {
 
     console.log("main events search Ctrl");
 
     $scope.loading = true;
-    $meteor.subscribe('search-events', {region:{id:1}} ).then(function(subscriptionHandler){
-        $scope.events = $meteor.collection(Events)
+    //$meteor.subscribe('search-events', {region:{id:1}} ).then(function(subscriptionHandler){
+        //$scope.events = $meteor.collection(Events)
+        //$scope.loading = false;
+    //});
+    
+    CollectionService.subscribe('search-events', {region:{id:1}}). then(function(events) {
+        $scope.events = events;
         $scope.loading = false;
     });
-
 
 
 });
