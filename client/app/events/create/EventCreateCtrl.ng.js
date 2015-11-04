a12bb48f5967343df80e901b2f547161ec829284
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-04 14:21:32 +0100

    Update EventCreateCtrl.ng.js

--- client/app/events/create/EventCreateCtrl.ng.js
+++ client/app/events/create/EventCreateCtrl.ng.js
@@ -1,15 +1,16 @@
 angular.module('entraide').controller('EventCreateCtrl', function ($rootScope, $scope, $meteor, $state, $stateParams, CollectionService, SessionService) {
 
     $scope.event = $stateParams.event;
+    
+    CollectionService.subscribe('my-events').then(function(events){ 
+        $scope.events = events; 
+    }); 
 
     $scope.create = function(event){
-        event.owner= {id:$scope.$root.currentUser._id};
-        CollectionService.subscribe('my-events').then(function(events){
-            events.push(event);
-            $rootScope.$broadcast('anim-sidebar-toggle');
-            $state.go("app.main.events.search.myEvents");
-        });
-    }
+        $scope.events.push(event);
+        $rootScope.$broadcast('anim-sidebar-toggle');
+        $state.go("app.main.events.search.myEvents");
+    };
 
 });
 
