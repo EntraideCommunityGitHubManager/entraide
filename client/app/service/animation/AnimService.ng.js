aa7e1df60158e854f3ba786f412169d62b394d33
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-09 13:00:31 +0100

    Update AnimService.ng.js

--- client/app/service/animation/AnimService.ng.js
+++ client/app/service/animation/AnimService.ng.js
@@ -1,19 +1,11 @@
 angular.module("entraide").factory("AnimService", function($rootScope){
 
     var animService = {
-        transition : {
-            toState: '',
-            fromState: '',
-            transitionning: false
-        },
         startTransition : function(delay){
             setTimeout(function(){$rootScope.$broadcast('anim-transition-start');}, delay ? delay : 1000);
         },
         stopTransition : function(delay) {
             setTimeout(function(){$rootScope.$broadcast('anim-transition-stop');}, delay ? delay : 2000);
-        },
-        isTransitionning: function(param){
-            return param.toState == this.transition.toState && param.fromState == this.transition.toState && this.transition.transitionning;
         }
     };
 
