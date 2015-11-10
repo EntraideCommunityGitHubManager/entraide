186758a070d74025dadd7ca4cec0e46da8b1d646
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-10 15:31:03 +0100

    Update AnimService.ng.js

--- client/app/service/animation/AnimService.ng.js
+++ client/app/service/animation/AnimService.ng.js
@@ -1,31 +1,31 @@
 angular.module("entraide").factory("AnimService", function($rootScope, $state){
 
     var animService = {
-		transitionConfig: {
-			type: 'parallelogram',
-			delay: 2000,
-			transitionning: false,
-			transition: null
-		},
-		getTransitionConfig: function(){
-			return this.transitionConfig;
-		}
-		isTransitionning: function(){
-			return this.transitionConfig.transitionning;
-		},
-		isNotCurrent: function(transition){
-			return !_.equals(transition, this.transitionConfig.transition);
-		},
-        startTransition : function(transition, type){
-			var service = this;
-			service.transitionConfig.transition = transition;
-			service.transitionConfig.type = type ? type : service.transitionConfig.type;
-			service.transitionConfig.transitionning = true;
-            $rootScope.$broadcast('anim-transition-start');
-			
-            setTimeout(function(){
-				service.transitionConfig.transitionning = false;
-			}, service.transitionConfig.delay);
+	transitionConfig: {
+		type: 'parallelogram',
+		delay: 2000,
+		transitionning: false,
+		transition: null
+	},
+	getTransitionConfig: function(){
+		return this.transitionConfig;
+	}
+	isTransitionning: function(){
+		return this.transitionConfig.transitionning;
+	},
+	isNotCurrent: function(transition){
+		return !_.equals(transition, this.transitionConfig.transition);
+	},
+	startTransition : function(transition, type){
+		var service = this;
+		service.transitionConfig.transition = transition;
+		service.transitionConfig.type = type ? type : service.transitionConfig.type;
+		service.transitionConfig.transitionning = true;
+    		$rootScope.$broadcast('anim-transition-start');
+		
+    		setTimeout(function(){
+			service.transitionConfig.transitionning = false;
+		}, service.transitionConfig.delay);
 			
         },
         stopTransition : function(delay) {
