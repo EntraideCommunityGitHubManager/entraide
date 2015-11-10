c0490d9e1038a15c2d830833d19da1506f8e1e9f
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-10 15:30:12 +0100

    Update AnimService.ng.js

--- client/app/service/animation/AnimService.ng.js
+++ client/app/service/animation/AnimService.ng.js
@@ -1,8 +1,32 @@
-angular.module("entraide").factory("AnimService", function($rootScope){
+angular.module("entraide").factory("AnimService", function($rootScope, $state){
 
     var animService = {
-        startTransition : function(delay){
-            setTimeout(function(){$rootScope.$broadcast('anim-transition-start');}, delay ? delay : 1000);
+		transitionConfig: {
+			type: 'parallelogram',
+			delay: 2000,
+			transitionning: false,
+			transition: null
+		},
+		getTransitionConfig: function(){
+			return this.transitionConfig;
+		}
+		isTransitionning: function(){
+			return this.transitionConfig.transitionning;
+		},
+		isNotCurrent: function(transition){
+			return !_.equals(transition, this.transitionConfig.transition);
+		},
+        startTransition : function(transition, type){
+			var service = this;
+			service.transitionConfig.transition = transition;
+			service.transitionConfig.type = type ? type : service.transitionConfig.type;
+			service.transitionConfig.transitionning = true;
+            $rootScope.$broadcast('anim-transition-start');
+			
+            setTimeout(function(){
+				service.transitionConfig.transitionning = false;
+			}, service.transitionConfig.delay);
+			
         },
         stopTransition : function(delay) {
             setTimeout(function(){$rootScope.$broadcast('anim-transition-stop');}, delay ? delay : 2000);
