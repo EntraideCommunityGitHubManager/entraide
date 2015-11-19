2d0896ff0eda1e1c1356d24cea03930d410fa62c
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-19 14:54:08 +0100

    Update app.ng.js

--- client/lib/app.ng.js
+++ client/lib/app.ng.js
@@ -283,82 +283,25 @@ angular.module('entraide').run(["$rootScope", "$urlRouter", "$state", "AnimServi
     });
 
     $rootScope.$on('$stateChangeStart', function(event, toState, toParams, fromState, fromParams) {
-        
-        var config = {
-			'includes' : [
-				{
-					'from' : 'app.main',
-					'to' : 'app.main.events.search.*'
-				},
-				{
-					'from' : 'app.main.events.search.byProfile',
-					'to' : 'app.main.events.search.myEvents'
-				},
-				{
-					'from' : 'app.main.events.search.myEvents',
-					'to' : 'app.main.events.search.byProfile'
-				}
-			],
-			'excludes' : ['logout']
-		};
-        
-        var isTransitionnable = function(t, params){
-            for(var i= 0; i < config.excludes.length; i++){
-				if(config.excludes[i]==params.targetState){
-					return false;
-				}
-			}
-			
-			for(var i= 0; i < config.includes.length; i++){
-				var fromRule = config.includes[i].from;
-				var toRule = config.includes[i].to;
-				
-				var fromStar = false;
-				var toStar = false;
-				
-				if(fromRule.indexOf('*')>=0){
-					fromRule = fromRule.replace('*','');
-					fromStar = true;
-				}
-				
-				if(toRule.indexOf('*')>=0){
-					toRule = toRule.replace('*','');
-					toStar = true;
-				}
-				
-				if(fromStar){
-					if(t.fromState.name.indexOf(fromRule)>=0){
-						if(toStar){
-							if(t.toState.name.indexOf(toRule)>=0){
-								return true;
-							}
-						} else {
-							if(t.toState.name == toRule){
-								return true;
-							}
-						}
-					}
-					
-				} else {
-					if(t.fromState.name == fromRule){
-						if(toStar){
-							if(t.toState.name.indexOf(toRule)>=0){
-								return true;
-							}
-						} else {
-							if(t.toState.name == toRule){
-								return true;
-							}
-						}
-					}
-				}
-			}
-			return false;
-        }
 
+	var config = {
+		includes : [{
+				from : 'app.main',
+				to : 'app.main.events.search.*'
+			},{
+				from : 'app.main.events.search.byProfile',
+				to : 'app.main.events.search.myEvents'
+			},{
+				from : 'app.main.events.search.myEvents',
+				to : 'app.main.events.search.byProfile'
+			}],
+		excludes : ['logout']
+	};
+        
+       
         var transition = {toState: toState, fromState: fromState};
 
-        if(isTransitionnable(transition, toParams)){
+        if(AnimService.isTransitionnable(transition, toParams)){
             if(AnimService.isNotCurrent(transition)){
                 AnimService.startTransition(transition);
             }
