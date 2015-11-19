8d1490748be27ac6209cc65a951ce6f4c4e3e2d4
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-19 14:54:53 +0100

    Update AnimService.ng.js

--- client/app/service/animation/AnimService.ng.js
+++ client/app/service/animation/AnimService.ng.js
@@ -43,6 +43,61 @@ angular.module("entraide").factory("AnimService", function($rootScope, $state){
             setTimeout(function(){
                 $rootScope.$broadcast('anim-transition-stop');
             }, delay ? delay : 1500);
+        },
+        isTransitionnable : function(t, params) {
+            var config = this.getRoutingConfig();
+            for(var i= 0; i < config.excludes.length; i++){
+				if(config.excludes[i]==params.targetState){
+					return false;
+				}
+			}
+			
+			for(var i= 0; i < config.includes.length; i++){
+				var fromRule = config.includes[i].from;
+				var toRule = config.includes[i].to;
+				
+				var fromStar = false;
+				var toStar = false;
+				
+				if(fromRule.indexOf('*')>=0){
+					fromRule = fromRule.replace('*','');
+					fromStar = true;
+				}
+				
+				if(toRule.indexOf('*')>=0){
+					toRule = toRule.replace('*','');
+					toStar = true;
+				}
+				
+				if(fromStar){
+					if(t.fromState.name.indexOf(fromRule)>=0){
+						if(toStar){
+							if(t.toState.name.indexOf(toRule)>=0){
+								return true;
+							}
+						} else {
+							if(t.toState.name == toRule){
+								return true;
+							}
+						}
+					}
+					
+				} else {
+					if(t.fromState.name == fromRule){
+						if(toStar){
+							if(t.toState.name.indexOf(toRule)>=0){
+								return true;
+							}
+						} else {
+							if(t.toState.name == toRule){
+								return true;
+							}
+						}
+					}
+				}
+			}
+			return false;
+
         }
 
     };
