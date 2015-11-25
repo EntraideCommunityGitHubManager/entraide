f5a94bae2fca255cff2041bc9930e4a6f02f9da2
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-25 13:49:58 +0100

    Update Events.js

--- client/app/model/Events.js
+++ client/app/model/Events.js
@@ -1,1 +1,11 @@
 Events = new Mongo.Collection("events");
+ProfileImages = new FS.Collection("profile_img", {
+  stores: [
+    new FS.Store.GridFS("original")
+  ],
+  filter: {
+    allow: {
+      contentTypes: ['image/*']
+    }
+  }
+});
