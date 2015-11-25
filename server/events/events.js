ef789b68d6ec8940e913bf339bafca03a121ce93
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-25 14:17:32 +0100

    Update events.js

--- server/events/events.js
+++ server/events/events.js
@@ -41,6 +41,41 @@ Meteor.publish("search-events", function(options){
 
 
 
+Profiles = new Mongo.Collection("profiles");
+
+Profiles.allow({
+    insert: function (userId, profile) {
+        return isAdmin(userId) || userId && profile.owner.id === userId;
+    },
+    update: function (userId, profile, fields, modifier) {
+        return isAdmin(userId) || userId && profile.owner.id === userId;
+    },
+    remove: function (userId, profile) {
+        return isAdmin(userId) || userId && profile.owner.id === userId;
+    }
+});
+
+Meteor.publish("all-profiles", function(){
+    if(isAdmin(this.userId)){
+        return Profiles.find({});
+    } else {
+        this.stop();
+        return;
+    }
+});
+
+Meteor.publish("my-profile", function(){
+    return Profiles.find({'owner.id' :  this.userId});
+});
+
+
+
+
+
+
+
+
+
 ProfileImages = new FS.Collection("profile_img", {
   stores: [
     new FS.Store.GridFS("original")
