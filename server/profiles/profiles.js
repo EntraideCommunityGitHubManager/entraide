6582d97cdcf1f62151d3de1e0f7fea0adf06388d
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-11-26 16:20:35 +0100

    Update profiles.js

--- server/profiles/profiles.js
+++ server/profiles/profiles.js
@@ -29,7 +29,12 @@ Meteor.publish("my-profile", function(){
 
 ProfileImages = new FS.Collection("profile_images", {
   stores: [
-    new FS.Store.GridFS("original")
+    new FS.Store.GridFS("original"),
+    new FS.Store.GridFS("thumbnail", {
+          transformWrite: function(fileObj, readStream, writeStream) {
+            gm(readStream, fileObj.name()).resize('32', '32', '!').stream().pipe(writeStream);
+          }
+        })
   ],
   filter: {
     allow: {
