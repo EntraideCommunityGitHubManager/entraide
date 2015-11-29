b0734ba176bea4b0003c020325f31f4a63faae9b
Alexis on Mac <entraide.community.developer@gmail.com>
2015-11-29 19:47:37 +0100

    Model cleaning

--- client/app/model/Events.js
+++ client/app/model/Events.js
@@ -1,14 +1,1 @@
 Events = new Mongo.Collection("events");
-
-Profiles = new Mongo.Collection("profiles");
-
-ProfileImages = new FS.Collection("profile_images", {
-  stores: [
-    new FS.Store.GridFS("original")
-  ],
-  filter: {
-    allow: {
-      contentTypes: ['image/*']
-    }
-  }
-});
---
+++ client/app/model/Profiles.js
@@ -0,0 +1,12 @@
+Profiles = new Mongo.Collection("profiles");
+
+ProfileImages = new FS.Collection("profile_images", {
+  stores: [
+    new FS.Store.GridFS("original")
+  ],
+  filter: {
+    allow: {
+      contentTypes: ['image/*']
+    }
+  }
+});
