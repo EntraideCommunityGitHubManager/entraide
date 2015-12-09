69ace89d37dc82c8d5ad2016236d7dca6867f1ed
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-09 17:04:43 +0100

    Update categories.js

--- server/categories/categories.js
+++ server/categories/categories.js
@@ -9,20 +9,20 @@ Meteor.startup(function () {
      
       // Music
       Categories.insert({code: 'music', ancestors:[], parent:null, name:'Musique'});
-      Categories.insert({code: 'music-oth', ancestors:['music'], parent:'music', name:'Autres'});
+      Categories.insert({code: 'music-othr', ancestors:['music'], parent:'music', name:'Autres'});
       
       Categories.insert({code: 'music-wind', ancestors:['music'], parent:'music', name:'Instrument à vent'});
-      Categories.insert({code: 'music-wind-flt', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Flûtes'});
-      Categories.insert({code: 'music-wind-sax', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Saxophones'});
-      Categories.insert({code: 'music-wind-oth', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Autres'});
+      Categories.insert({code: 'music-wind-flut', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Flûtes'});
+      Categories.insert({code: 'music-wind-saxo', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Saxophones'});
+      Categories.insert({code: 'music-wind-othr', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Autres'});
       
       Categories.insert({code: 'music-elec', ancestors:['music'], parent:'music', name:'Instrument électronique'});
-      Categories.insert({code: 'music-elec-syn', ancestors:['music', 'music-elec'], parent:'music-elec', name:'Instrument électronique'});
-      Categories.insert({code: 'music-elec-oth', ancestors:['music', 'music-elec'], parent:'music-elec', name:'Autres'});
+      Categories.insert({code: 'music-elec-synt', ancestors:['music', 'music-elec'], parent:'music-elec', name:'Instrument électronique'});
+      Categories.insert({code: 'music-elec-othr', ancestors:['music', 'music-elec'], parent:'music-elec', name:'Autres'});
       
       Categories.insert({code: 'music-strg', ancestors:['music'], parent:'music', name:'Instrument à corde'});
-      Categories.insert({code: 'music-strg-vio', ancestors:['music', 'music-strg'], parent:'music-strg', name:'Violons'});
-      Categories.insert({code: 'music-strg-oth', ancestors:['music', 'music-strg'], parent:'music-strg', name:'Autres'});
+      Categories.insert({code: 'music-strg-viol', ancestors:['music', 'music-strg'], parent:'music-strg', name:'Violons'});
+      Categories.insert({code: 'music-strg-othr', ancestors:['music', 'music-strg'], parent:'music-strg', name:'Autres'});
       
       // Bricolage
       Categories.insert({code: 'brico', ancestors:[], parent:null, name:'Bricolage'});
