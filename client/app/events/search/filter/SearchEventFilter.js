9737fc490de7297afb5ef329a3ebfb1cb8ccdc43
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-09 14:11:52 +0100

    Create SearchEventFilter.js

---
+++ client/app/events/search/filter/SearchEventFilter.js
@@ -0,0 +1,8 @@
+angular.module('entraide').controller('SearchEventFilterCtrl', function ($rootScope, $scope, $meteor, $state, SessionService, SearchEventService) {
+
+    $scope.search = function(){
+        alert('search');
+    }
+
+});
+
