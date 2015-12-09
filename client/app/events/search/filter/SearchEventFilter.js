51fee7b66087543512f13656ec3376090b8e7635
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-09 15:19:56 +0100

    Update SearchEventFilter.js

--- client/app/events/search/filter/SearchEventFilter.js
+++ client/app/events/search/filter/SearchEventFilter.js
@@ -1,8 +1,12 @@
-angular.module('entraide').controller('SearchEventFilterCtrl', function ($rootScope, $scope, $meteor, $state, SessionService, SearchEventService) {
+angular.module('entraide').controller('SearchEventFilterCtrl', function ($rootScope, $scope, $meteor, $state, SessionService, CollectionService, SearchEventService) {
+
+    console.log('SearchEventFilterCtrl');
+    
+    $scope.categories = 
 
     $scope.search = function(){
         alert('search');
-    }
+    };
 
 });
 
