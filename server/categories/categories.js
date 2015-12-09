2ce6668c5cce7ae814d356f56c6ddb5f6109f6a4
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-09 15:59:01 +0100

    Update categories.js

--- server/categories/categories.js
+++ server/categories/categories.js
@@ -9,9 +9,9 @@ Meteor.startup(function () {
       Categories.insert({code: 'music', ancestors:[], parent:null, name:'Musique'});
       
       Categories.insert({code: 'music-wind', ancestors:['music'], parent:'music', name:'Instrument à vent'});
-      Categories.insert({code: 'music-wind-flt', ancestors:['music', 'music-wind'], parent:"music-wind", name:'Flûtes'});
-      Categories.insert({code: 'music-wind-sax', ancestors:['music', 'music-wind'], parent:"music-wind", name:'Saxophones'});
-      Categories.insert({code: 'music-wind-oth', ancestors:['music', "music-wind'], parent:"music-wind", name:'Autres'});
+      Categories.insert({code: 'music-wind-flt', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Flûtes'});
+      Categories.insert({code: 'music-wind-sax', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Saxophones'});
+      Categories.insert({code: 'music-wind-oth', ancestors:['music', 'music-wind'], parent:'music-wind', name:'Autres'});
       
       Categories.insert({code: 'music-elec', ancestors:['music'], parent:'music', name:'Instrument électronique'});
       Categories.insert({code: 'music-elec-syn', ancestors:['music', 'music-elec'], parent:'music-elec', name:'Instrument électronique'});
