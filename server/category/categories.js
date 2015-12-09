9813a27e794cb4f63aa1cad39b36b61537ce770f
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-09 15:14:25 +0100

    Update categories.js

--- server/category/categories.js
+++ server/category/categories.js
@@ -1,13 +1,1 @@
-Categories = new Mongo.Collection("categories");
 
-Meteor.publish("all-categories", function(){
-   return Categories.find();
-});
-
-Meteor.startup(function () {
-  if(Categories.find({}).count()<=0){
-      Categories.insert({"code":"MUSIC","name":"Musique"});
-      Categories.insert({"code":"BRICO","name":"Bricolage"});
-  }
-
-});
