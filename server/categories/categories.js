8bb71be0639bdee2e4425e0a01252f349b02f9b0
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-09 15:28:18 +0100

    Update categories.js

--- server/categories/categories.js
+++ server/categories/categories.js
@@ -1,12 +1,23 @@
 Categories = new Mongo.Collection("categories");
 
 Meteor.publish("all-categories", function(){
-   return Categories.find();
+   return Categories.find({});
 });
 
 Meteor.startup(function () {
   if(Categories.find({}).count()<=0){
-      Categories.insert({"code":"MUSIC","name":"Musique"});
+      Categories.insert({"code":"MUSIC", "name":"Musique",
+         categories : [{
+            code:'MUSIC-WIND',
+            name:'Instrument à vent'
+         },{
+            code:'MUSIC-ELEC',
+            name:'Instrument électronique' 
+         },{
+            code:'MUSIC-STRG',
+            name:'Instrument à corde' 
+         }]
+      });
       Categories.insert({"code":"BRICO","name":"Bricolage"});
   }
 
