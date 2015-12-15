d205b970a11c3d3d5bfac0962129e520fd82af2e
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-15 12:08:11 +0100

    Update anim-button.ng.js

--- client/app/common/directives/anim-button/anim-button.ng.js
+++ client/app/common/directives/anim-button/anim-button.ng.js
@@ -6,16 +6,27 @@ angular.module('entraide').directive('animButton', function(){
         scope: {
             animButtonIcon: '@',
             animButtonType: '@',
+            animButtonSubType: '@'
             animButtonCallback: '&'
         },
         templateUrl: 'client/app/common/directives/anim-button/anim-button.ng.html',
         controller: function($scope){
+            $scope.config = {
+                
+            };
         },
         link: function (scope, element) {
+            
+            scope.animButtonSubType = scope.animButtonSubType ? scope.animButtonSubType : '1';
             //var cbutton = element[0].querySelector('.animButton');
             scope.clickHandler = function(e){
                 scope.animButtonCallback();
             };
+            
+            scope.getClass = function(){
+            	return scope.config[scope.animButtonType].class + ' ' + scope.config[scope.animButtonType][scope.animButtonSubType];
+            };
+                        
             scope.$on("$destroy", function () {});
         }
     };
