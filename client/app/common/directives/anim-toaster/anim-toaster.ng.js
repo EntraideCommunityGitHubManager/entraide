fc0e5ff3ae74ac3bf38104b79366104475e3b248
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-17 15:02:58 +0100

    Update anim-toaster.ng.js

--- client/app/common/directives/anim-toaster/anim-toaster.ng.js
+++ client/app/common/directives/anim-toaster/anim-toaster.ng.js
@@ -1,11 +1,11 @@
-angular.module('entraide').directive('animToaster', function(){
+angular.module('entraide').directive('animToaster', function(AnimToasterNotificationService){
 
     return {
         restrict: 'AEC',
         replace: true,
         templateUrl: 'client/app/common/directives/anim-toaster/anim-toaster.ng.html',
         controller: function($scope){
-           
+           $scope.notifService = AnimToasterNotificationService;
         },
         link: function (scope, element) {
             scope.$on("$destroy", function () {});
