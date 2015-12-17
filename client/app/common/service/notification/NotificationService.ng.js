b7e59fef4a1b378606210999c18a4e8ca6f27ad5
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-17 11:33:11 +0100

    Update NotificationService.ng.js

--- client/app/common/service/notification/NotificationService.ng.js
+++ client/app/common/service/notification/NotificationService.ng.js
@@ -1,9 +1,14 @@
-angular.module("entraide").factory("NotificationService", function($rootScope, CollectionService, SecurityService, $q){
+angular.module("entraide").factory("NotificationService", function(){
 
     var notificationService = {
-        infoMessage: [],
-        successMessage: [],
-        errorMessage: []
+        infoQueue: [],
+        successQueue: [],
+        warningQueue: [],
+        errorQueue: [],
+        infoMessage: null,
+        successMessage: null,
+        warningMessage: null,
+        errorMessage: null
     };
     
     return notificationService;
