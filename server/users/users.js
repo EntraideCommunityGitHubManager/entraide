cc8a900a7dac82ff00b421d539b11487d2a4edeb
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-17 11:01:10 +0100

    Update users.js

--- server/users/users.js
+++ server/users/users.js
@@ -95,23 +95,3 @@ isAdmin = function(userId){
     }
     return isAdmin;
 };
-
-setStringValue=function(str, length){
-    return str ? str.substring(0, length) : null;
-};
-
-setIntValue= function(val){
-    return isNaN(parseInt(val)) ? null : parseInt(val);
-};
-
-setFloatValue= function(val){
-    return isNaN(parseFloat(val)) ? null : parseFloat(val);
-};
-
-setDateValue= function(val){
-    return isNaN(parseInt(val)) ? null : parseInt(val);
-};
-
-String.prototype.capitalize = function() {
-    return this.charAt(0).toUpperCase() + this.slice(1);
-};
