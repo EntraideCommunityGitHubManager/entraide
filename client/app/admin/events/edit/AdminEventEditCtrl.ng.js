3ea3e1879fcf53554b3523bc929ffef7573ac3c7
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-18 15:46:07 +0100

    Update AdminEventEditCtrl.ng.js

--- client/app/admin/events/edit/AdminEventEditCtrl.ng.js
+++ client/app/admin/events/edit/AdminEventEditCtrl.ng.js
@@ -1,4 +1,4 @@
-angular.module('entraide').controller('AdminEventEditCtrl', function ($scope, $meteor, $stateParams, $state, CollectionService) {
+angular.module('entraide').controller('AdminEventEditCtrl', function ($scope, $meteor, $stateParams, $state, CollectionService, AnimToasterNotificationService) {
 
     CollectionService.subscribe('all-events').then(function(events){
         $scope.events = events;
@@ -6,11 +6,21 @@ angular.module('entraide').controller('AdminEventEditCtrl', function ($scope, $m
     });
 
     $scope.update = function(event){
-        event.save().then(function(){$scope.back();},function(error){alert(error);});
+        event.save().then(function(){
+            AnimToasterNotificationService.addSuccessMessage("The event has been succcesfully changed.");
+            $scope.back();
+        },function(error){
+            AnimToasterNotificationService.addErrorMessage("Error : " error.reason);
+        });
     };
 
     $scope.remove = function(event){
-        $scope.events.remove(event).then(function(){$scope.back();},function(err){alert(err);});
+        $scope.events.remove(event).then(function(){
+            AnimToasterNotificationService.addSuccessMessage("The event has been succcesfully deleted.");
+            $scope.back();
+        },function(error){
+            AnimToasterNotificationService.addErrorMessage("Error : " error.reason);
+        });
     };
 
     $scope.back = function(){
