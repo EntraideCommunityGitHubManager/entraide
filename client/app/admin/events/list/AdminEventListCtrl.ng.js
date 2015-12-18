00117c17cda19b0777dabaa78e7326c1600e7cf2
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-18 16:21:51 +0100

    Update AdminEventListCtrl.ng.js

--- client/app/admin/events/list/AdminEventListCtrl.ng.js
+++ client/app/admin/events/list/AdminEventListCtrl.ng.js
@@ -1,13 +1,15 @@
 angular.module('entraide').controller('AdminEventListCtrl', function ($scope, $meteor, CollectionService) {
 
-    $scope.loading = true;
     CollectionService.subscribe('all-events').then(function(events) {
         $scope.events = events;
-        $scope.loading = false;
     });
 
     $scope.remove = function(event){
-        $scope.events.remove(event);
+        $scope.events.remove(event).then(function(){
+            AnimToasterNotificationService.addSuccessMessage("The event has been succcesfully removed.");
+        },function(error){
+            AnimToasterNotificationService.addErrorMessage("Error : " error.reason);
+        });
     };
 
 });
