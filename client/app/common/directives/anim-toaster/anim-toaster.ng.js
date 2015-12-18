8c0f73649df020b6a5d2ae764e44fb99edb74d58
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2015-12-18 10:12:58 +0100

    Update anim-toaster.ng.js

--- client/app/common/directives/anim-toaster/anim-toaster.ng.js
+++ client/app/common/directives/anim-toaster/anim-toaster.ng.js
@@ -6,13 +6,13 @@ angular.module('entraide').directive('animToaster', function(AnimToasterNotifica
         templateUrl: 'client/app/common/directives/anim-toaster/anim-toaster.ng.html',
         controller: function($scope){
            $scope.service = AnimToasterNotificationService;
+           angular.$watch('service.infoMessage', refresh());
+           angular.$watch('service.successMessage', refresh());
+           angular.$watch('service.warningMessage', refresh());
+           angular.$watch('service.errorMessage', refresh());
+           function refresh(){setTimeout(function(){$scope.$digest();},100);}
         },
         link: function (scope, element) {
-            angular.$watch('service.infoMessage', refresh());
-            angular.$watch('service.successMessage', refresh());
-            angular.$watch('service.warningMessage', refresh());
-            angular.$watch('service.errorMessage', refresh());
-            function refresh(){setTimeout(function(){scope.$digest();},100);}
             scope.$on("$destroy", function () {});
         }
     };
