d6befa72c06f16bcc6a774991f3babaf19fd04e2
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2016-08-19 15:37:25 +0200

    Update EventCreateCtrl.ng.js

--- client/app/events/create/EventCreateCtrl.ng.js
+++ client/app/events/create/EventCreateCtrl.ng.js
@@ -1,8 +1,15 @@
 angular.module('entraide').controller('EventCreateCtrl', function ($rootScope, $scope, $meteor, $state, $stateParams, $timeout, CollectionService, AnimService, MapService, AnimToasterNotificationService) {
 
     $scope.event = $stateParams.event;
-    $scope.selectedCategory = null;
     MapService.addMarker($scope.event.location);
+    
+    $scope.categoryFilter = { 
+        filterTerm: "", 
+        categories: [],
+        categoriesDisplayed: [],
+        categoriesSelected: [],
+        categoriesSelectedRating: {}
+    };
     
     CollectionService.subscribe('my-events').then(function(events){ 
         $scope.events = events;
@@ -10,15 +17,20 @@ angular.module('entraide').controller('EventCreateCtrl', function ($rootScope, $
     });
 
     CollectionService.subscribe('all-categories').then(function(categories){
-        $scope.categories = categories;
+        $scope.categoryFilter.categories = categories;
     });
 
     $scope.create = function(event){
-        event.icon = 'category/music-elec-drum/music-elec-drum.png';
-        $meteor.call('event_create', event).then(function(id){
-            MapService.removeAddedMarker();
-            AnimToasterNotificationService.addSuccessMessage("L'évenement a été ajouté avec succès !");
-            $state.go("app.main.events.search.myEvents.edit", {"event" : {_id: id}});
+        $meteor.call('event_create', event).then(function(eventId){
+            var skills = [];
+            angular.forEach($scope.categoryFilter.categoriesSelected, function(cat){
+                skills.push({eventId:eventId, categoryCode:cat.code, level: $scope.categoryFilter.categoriesSelectedRating[cat.code]});
+            });
+            $meteor.call('event_skill_create', skills).then(function(skillsId){
+                MapService.removeAddedMarker();
+                AnimToasterNotificationService.addSuccessMessage("L'évenement a été ajouté avec succès !");
+                $state.go("app.main.events.search.myEvents.edit", {"event" : {_id: eventId}});
+            },function(err){$scope.error=err;});  
         },function(err){$scope.error=err;});
     };
 
