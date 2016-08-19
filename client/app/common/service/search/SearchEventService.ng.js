5aa21e1b2c094faf3f10a5fb8847bd2a2aa3bb40
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2016-08-19 13:18:11 +0200

    Update SearchEventService.ng.js

--- client/app/common/service/search/SearchEventService.ng.js
+++ client/app/common/service/search/SearchEventService.ng.js
@@ -1,7 +1,25 @@
-angular.module("entraide").factory("SearchEventService", function($rootScope, CollectionService, $q){
+angular.module("entraide").factory("SearchEventService", function($rootScope, CollectionService, SessionService, $q){
+
+    var department = SessionService.getUserProfile().department;
+    var options = {collectionOptions:{'department.code': department.code}, skillsOptions:[], backend:true};
 
     var searchEventService = {
-        searchEventFilter: { searchTerm: "", filterTerm: "", categories: []}
+        searchEventFilter: { 
+            searchTerm: "", 
+            filterTerm: "", 
+            categories: [],
+            categoriesDisplayed: [],
+            categoriesSelected: [],
+            categoriesSelectedRating: []
+        },
+        searchEvents: function(){
+            var service = this;
+            options.skillsOptions = [];
+            angular.forEach(service.categoriesSelected, function(cat, i){
+                options.skillsOptions.push({code:cat.code, level: service.categoriesSelectedRating[i]});
+            });
+            return CollectionService.subscribe('search-events', options);
+        }
     };
 
     return searchEventService;
