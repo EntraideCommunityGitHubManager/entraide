9da35eeb152cb27895c610c1ed4dc33fc4001796
EntraideCommunityGitHubManager <EntraideCommunityGitHubManager@users.noreply.github.com>
2016-08-24 10:52:47 +0200

    Update ChainPromiseFactory.ng.js

--- client/app/common/service/utils/ChainPromiseFactory.ng.js
+++ client/app/common/service/utils/ChainPromiseFactory.ng.js
@@ -1,8 +1,8 @@
 angular.module('entraide').factory('ChainPromiseFactory', [function () {
     // Example of use :
     //var chain = $q.when();
-    //angular.forEach(roles, function(role) {
-    //    chain = chain.then(ChainPromiseFactory.chain(UserService.addRole, [user.id, role.id], successCallback, errorCallback));
+    //angular.forEach(roles, function(role, i) {
+    //    chain = chain.then(ChainPromiseFactory.chain(UserService.addRole, [user.id, role.id], successCallback, errorCallback, i==roles.length));
     //});
     return {
         chain: function(method, params, callbackSuccess, callbackError, csc, cec) {
